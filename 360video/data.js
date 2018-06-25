be28fc25634053c41c3ea817962fa1635574a241
moscato622 <moscato6226790@gmail.com>
2018-06-25 15:25:05 -0400

    combineTest

---
+++ 360video/data.js
@@ -0,0 +1,51 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "全景图",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.3561912406919348,
+          "pitch": -0.1998982912661198,
+          "title": "White Mountain",
+          "text": "Big<div>Salt</div><div>Mountain</div>"
+        },
+        {
+          "yaw": -1.1439120619561631,
+          "pitch": 0.15653584895098227,
+          "title": "Salt Lake",
+          "text": "Wide<div>Blue</div><div>Green</div><div>Lake</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-",
-      "name": "全景图",
+      "id": "0-ss",
+      "name": "ss",
       "levels": [
         {
           "tileSize": 256,
@@ -12,39 +12,22 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 341.5,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
-      "infoHotspots": [
-        {
-          "yaw": 1.3561912406919348,
-          "pitch": -0.1998982912661198,
-          "title": "White Mountain",
-          "text": "Big<div>Salt</div><div>Mountain</div>"
-        },
-        {
-          "yaw": -1.1439120619561631,
-          "pitch": 0.15653584895098227,
-          "title": "Salt Lake",
-          "text": "Wide<div>Blue</div><div>Green</div><div>Lake</div>"
-        }
-      ]
+      "infoHotspots": []
     }
   ],
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": false,
+    "autorotateEnabled": true,
     "fullscreenButton": false,
     "viewControlButtons": false
   }
