e9615b91fdc670f7a146fb6cb4d4aba5b2b2a35f
moscato622 <moscato6226790@gmail.com>
2018-06-26 19:05:41 -0400

    combineTest

--- index.js
+++ index.js
@@ -47,17 +47,20 @@ var APP_DATA = {
   }
 };
 
+  var clicked = false;
+
 (function() {
   var data = APP_DATA;
   var Marzipano = window.Marzipano;
 
   var panoElement = document.getElementById("pano");
   var viewerOpts = {
+    stageType: 'webgl',
     controls: {
       mouseViewMode: data.settings.mouseViewMode
     }
   };
-
+  var viewerOpts = { stageType: 'webgl' };
   var viewer = new Marzipano.Viewer(panoElement, viewerOpts);
 
   var levels = [{ tileSize: 256, size: 256 }, { tileSize: 512, size: 512 }];
@@ -69,72 +72,16 @@ var APP_DATA = {
     "tiles/0-ss/{z}/{f}/{y}/{x}.jpg"
   );
   var view = new Marzipano.RectilinearView();
-  var scene = viewer.createScene({
-     source: source,
-    geometry: geometry,
-    view: view
-  });
+  
 
   //////scene 02
   // Initialize viewer.
-    var viewerOpts2 = {
-    stageType: 'webgl',
-    controls: {
-      mouseViewMode: data.settings.mouseViewMode
-    }
-  };
+ 
 
-  var viewerOpts2 = { stageType: 'webgl' };
-  var viewer2 = new Marzipano.Viewer(document.getElementById('pano'), viewerOpts2);
-
+  ////Initaialize video asset
   var asset = new VideoAsset();
-  var source2 = new Marzipano.SingleAssetSource(asset);
-  var geometry2 = new Marzipano.EquirectGeometry([ { width: 1 } ]);
-  
-  var limiter = Marzipano.RectilinearView.limit.vfov(90*Math.PI/180, 90*Math.PI/180);
-  var view2 = new Marzipano.RectilinearView({ fov: Math.PI/2 }, limiter);
 
-
-
-  var scene2 = viewer2.createScene({
-    source: source2,
-    geometry: geometry2,
-    view: view2
-  });
-
-  ////
-
-  var element = document.getElementById("spot");
-  var position = { yaw: Math.PI / 4, pitch: Math.PI / 8 };
-  scene.hotspotContainer().createHotspot(element, position);
-
-  var toScene01Button = document.getElementById("to-scene1");
-  var toScene02Button = document.getElementById("to-scene2");
-
-  toScene01Button.addEventListener("click", function() {
-    scene.switchTo({
-      transitionDuration: 1000
-    });
-  });
-
-  toScene02Button.addEventListener("click", function() {
-    scene2.switchTo({
-      transitionDuration: 1000
-    });
-    play360Video();
-  });
-
-  scene.switchTo({
-    transitionDuration: 1000
-  });
-})();
-
-function play360Video(){
-// Try to start playback.
-function tryStart() {
-
-
-  var video = document.createElement('video');
+    var video = document.createElement('video');
   video.src = 'http://www.marzipano.net/media/video/mercedes-f1-1280x640.mp4';
   video.crossOrigin = 'anonymous';
   video.autoplay = true;
@@ -144,25 +91,90 @@ function tryStart() {
   video.playsInline = true;
   video.webkitPlaysInline = true;
 
+  //play video and apply it to the source
   video.play();
+  asset.setVideo(video);
 
-  waitForReadyState(video, video.HAVE_METADATA, 100, function() {
-    waitForReadyState(video, video.HAVE_ENOUGH_DATA, 100, function() {
-      asset.setVideo(video);
-    });
+  //video scene parameters
+  var source2 = new Marzipano.SingleAssetSource(asset);
+  var geometry2 = new Marzipano.EquirectGeometry([ { width: 1 } ]);
+  
+  var limiter = Marzipano.RectilinearView.limit.vfov(90*Math.PI/180, 90*Math.PI/180);
+  var view2 = new Marzipano.RectilinearView({ fov: Math.PI/2 }, limiter);
+
+  //scene 1
+  var scene = viewer.createScene({
+    source: source2,
+    geometry: geometry2,
+    view: view2,
+    pinFirstLevel: true
   });
-}
+
+  //scene 2
+  var scene2 = viewer.createScene({
+    source: source2,
+    geometry: geometry2,
+    view: view2,
+    pinFirstLevel: true
+  });
+
+  ////
+  
+  var element = document.getElementById("spot");
+  var position = { yaw: Math.PI / 4, pitch: Math.PI / 8 };
+  scene.hotspotContainer().createHotspot(element, position);
+
+  var toScene01Button = document.getElementById("to-scene1");
+  var toScene02Button = document.getElementById("to-scene2");
+
+  toScene01Button.addEventListener("mouseup", function() {
+   scene.switchTo();
+  });
+
+  toScene02Button.addEventListener("mouseup", function() {
+  	
+    scene2.switchTo();
+    
+  });
+   
+  // scene.switchTo({
+  //   transitionDuration: 1000
+  // });
+  scene2.switchTo();
+    console.log("Hello world!");
+
+
+  // var started = false;
+// Try to start playback.
+  
+  // if (started) {
+  //   return;
+  // }
+  // started = true;
+
+
+  // waitForReadyState(video, video.HAVE_METADATA, 100, function() {
+  //   waitForReadyState(video, video.HAVE_ENOUGH_DATA, 100, function() {
+  //     asset.setVideo(video);
+  //   });
+  // });
+
+
+  
+})();
+
+
+
 
 // Wait for an element to reach the given readyState by polling.
 // The HTML5 video element exposes a `readystatechange` event that could be
 // listened for instead, but it seems to be unreliable on some browsers.
-function waitForReadyState(element, readyState, interval, done) {
-  var timer = setInterval(function() {
-    if (element.readyState >= readyState) {
-      clearInterval(timer);
-      done(null, true);
-    }
-  }, interval);
-};
-
-};
+// function waitForReadyState(element, readyState, interval, done) {
+//   var timer = setInterval(function() {
+//     if (element.readyState >= readyState) {
+//       clearInterval(timer);
+//       done(null, true);
+//     }
+//   }, interval);
+// }
+//}
